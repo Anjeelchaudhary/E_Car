4862a57a638876ac97f72386e57b75b87e217aef
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-07-28 22:02:21 +0545

    added updated Files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './container/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default function App() { 
+  return (
+
+    <div>
+      
+      <div> 
+        <Header></Header>
+        <Home></Home>
+        <Footer></Footer>
+      </div>
+     
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav>
+        <div className='logo'>MyBrand</div>
+    </nav>
+  )
+}

c92215830f9997bfc5993e5de5853bd475dfab05
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-07-30 22:47:17 +0545

    Routs and BrowseRoutes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Route , BrowserRouter } from 'react-router-dom'
+import Home from '../container/Home'
 
 export default function Header() {
   return (
     <nav>
         <div className='logo'>MyBrand</div>
-        <img src="/Users/anjeelchaudhary/workingRobo/Repos/ClothingHouse/public/images/logo" alt="" />
+        <img src=" " alt="" />
         <div className='explore'>
           <h1>Explore</h1>
+          <BrowserRouter>
+            <Route>
+              <Route path='/' element={<Home />}/>
+            </Route>
+          </BrowserRouter>
+          
           <ul>
             <li>Categories</li>
             <li>Best Offers</li>

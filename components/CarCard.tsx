fef48026c678a583fb615970a7e78d0557ded522
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-09-01 15:40:21 +0545

    configuring for deployment

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,20 +1,23 @@
 "use client";
 
-import { useState } from "react"
-import Image from "next/image"
-import { CarProps } from "@/types"
-import {calculateCarRent ,generateCarImageUrl} from "@/utils"
+import { useState } from "react";
+import Image from "next/image";
+
 import CustomButton from "./CustomButton";
 import CarDetails from "./CarDetails";
+import { CarProps } from "@/types";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 
 interface CarCardProps {
   car: CarProps;
 }
 
-const CarCard = ({car}: CarCardProps) => {
-  const {city_mpg ,year , make , model , transmission , drive} = car;
-  const CarRent = calculateCarRent(city_mpg,year);
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
   const [isOpen, setIsOpen] = useState(false);
+
+  const carRent = calculateCarRent(city_mpg, year);
 
   return (
     <div className="car-card group">
@@ -23,52 +26,49 @@ const CarCard = ({car}: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
-      <p className="flex mt-6 text-[32px] font-extrabold">
-        <span className="self-start text-[14px] font-semibold">
-          $
-        </span>
-        {CarRent}
-        <span className="self-end text-[14px] font-medium">
-          /day
-        </span>
-      </p>
-      <div className="relative w-full h-40 my-3 object-contain">
-        <Image src={generateCarImageUrl(car)} alt="car model"
-         fill priority className="object-contain"/>
-      </div>
-      <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible w-full justify-between text-gray">
-          <div className="flex felx-col justify-center items-center gap-2">
-            <Image src="/steering-wheel.svg" width={20} height={20} alt="steer"/>
-            <p className="text-[14px]">
-              {transmission == 'a' ? 'Automatic' : 'Manual'}  
-            </p>
-          </div>
-        </div>
-        <div className="flex group-hover:invisible w-full justify-between text-gray">
-          <div className="flex felx-col justify-center items-center gap-2">
-            <Image src="/tire.svg" width={20} height={20} alt="tire"/>
-            {drive.toUpperCase()}
-          </div>
-        </div>
-        <div className="flex group-hover:invisible w-full justify-between text-gray">
-          <div className="flex felx-col justify-center items-center gap-2">
-            <Image src="/gas.svg" width={20} height={20} alt="gas"/>
-            <p className="text-[14px]">
-              {city_mpg} MPG  
-            </p>
-          </div>
-        </div>
-        <div className="car-card__btn-container">
-          <CustomButton title="View More" containerStyles="w-full py-[16px]rounded-full bg-primary-blue" 
-          textStyles="text-white text-[14] leading-[17px] font-bold" 
-          rightIcon="/right-arrow.svg" 
-          handleClick={() => setIsOpen(true)}/>
-        </div>
-      </div>
-      <CarDetails isOpen={isOpen} closeModel={() => setIsOpen(false)} car={car}  />
-    </div>
-  )
-}
 
-export default CarCard
+      <p className='flex mt-6 text-[32px] leading-[38px] font-extrabold'>
+        <span className='self-start text-[14px] leading-[17px] font-semibold'>$</span>
+        {carRent}
+        <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
+      </p>
+
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
+      </div>
+
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-grey'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
+            <p className='text-[14px] leading-[17px]'>
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="car-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
+          </div>
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
+          </div>
+        </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title='View More'
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            textStyles='text-white text-[14px] leading-[17px] font-bold'
+            rightIcon='/right-arrow.svg'
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+
+      <CarDetails isOpen={isOpen} closeModel={() => setIsOpen(false)} car={car} />
+    </div>
+  );
+};
+
+export default CarCard;

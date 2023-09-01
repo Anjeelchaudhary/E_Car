510052a47d7aff2285383cf8eff0491de3d67638
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-09-01 16:00:59 +0545

    Navbar configured

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,28 @@
-import Link from "next/link"
-import Image from "next/image"
-import { CustomButton } from "."
+import Link from "next/link";
+import Image from "next/image";
 
-const Navbar = () => {
-    return (
-        <header className="w-full absolute z-10">
-            <nav className="max-w-[1440px] mx-auto flex justify-between items-center">
-                <Link href="/" className="flex justify-center items-center ">
-                    <Image
-                        src="./logo.svg"
-                        alt="carLogo"
-                        width={118}
-                        height={18}
-                        className="object-contain" />
-                </Link>
-                <CustomButton
-                    title="Login"
-                    btnType="button"
-                    containerStyles="text-primary-blue rounded-full bg-white min-w-[130]" />
-            </nav>
-        </header>
-    )
-}
+import CustomButton from "./CustomButton";
 
-export default Navbar
+const NavBar = () => (
+  <header className='w-full  absolute z-10'>
+    <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
+      <Link href='/' className='flex justify-center items-center'>
+        <Image
+          src='/logo.svg'
+          alt='logo'
+          width={118}
+          height={18}
+          className='object-contain'
+        />
+      </Link>
+
+      <CustomButton
+        title='Sign in'
+        btnType='button'
+        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+      />
+    </nav>
+  </header>
+);
+
+export default NavBar;
